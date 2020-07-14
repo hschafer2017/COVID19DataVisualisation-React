8a31ac7c6ddb75aca567030b69ec468751970415
hschafer2017 <hschafer@umich.edu>
2020-07-14 23:05:46 +0100

    moved country dropdown to App.js, country grid is updating per country based on country chosen in drop down

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Dropdown } from 'semantic-ui-react';
 import './App.css';
 import Countries from './components/countryMap';
 import CountryGrid from './components/perCountryGrid';
-import CountrySearch from './components/countrySearch';
+import countryOptions from './components/countryOptions';
 
 
 class App extends Component {
@@ -13,31 +14,50 @@ class App extends Component {
 		this.state = {
 			countryData: [],
 			oneCountryData: [],
+			countryValue: "",
 			loaded: false,
 			placeholder: "Loading..."
 		};
 	};
 
 	componentDidMount() {
-		Promise.all([
-			fetch('https://api.covid19api.com/summary'),
-			fetch('https://api.covid19api.com/total/country/united-states')
-		])
-		.then(([response, response1]) => Promise.all([response.json(), response1.json()]))
-		.then(([response, response1]) => {
+		fetch('https://api.covid19api.com/summary')
+		.then(response => response.json())
+		.then(response => {
 			this.setState({
 				countryData: response.Countries,
-				oneCountryData: response1
 			});
 		})
 		.catch(console.log)
 	};
 
+	handleSelectChange = (e, {value}) => {
+		this.setState({
+            countryValue: value
+		});
+		fetch(`https://api.covid19api.com/total/country/${value}`)
+		.then(countryResponse => countryResponse.json())
+		.then(countryResponse => {
+			this.setState({
+				oneCountryData: countryResponse
+			})
+		})
+		.catch(console.log)
+	};
+
 	render () {
+		const { value } = this.state.countryValue;
 		return (
 			<div>
 				<Countries countryData={this.state.countryData} />
-				<CountrySearch />
+				<Dropdown
+					placeholder='Select Country'
+					search
+					selection
+					options={countryOptions}
+					value={value}
+					onChange={this.handleSelectChange}
+            	/>
 				<CountryGrid eachCountryData={this.state.oneCountryData} />
 			</div>
 		)
